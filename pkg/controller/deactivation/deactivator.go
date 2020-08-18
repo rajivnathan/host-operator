0e0dcca3ef982d2d92982c9cda50be40986e590c
Rajiv Senthilnathan <rsenthil@redhat.com>
2020-08-18 17:23:02 -0400

    Clean up

--- pkg/controller/deactivation/deactivator.go
+++ pkg/controller/deactivation/deactivator.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-	errs "github.com/pkg/errors"
+	"github.com/go-logr/logr"
 
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
@@ -25,94 +25,102 @@ var log = logf.Log.WithName("deactivator")
 func StartDeactivator(mgr manager.Manager, namespace string, stopChan <-chan struct{}, period time.Duration) {
 	log.Info("starting deactivator", "period", period)
 	go wait.Until(func() {
-		if err := checkUserRecordsForDeactivation(namespace, mgr.GetClient()); err != nil {
-			fmt.Println(err.Error())
+		deactivator := deactivator{
+			client:    mgr.GetClient(),
+			namespace: namespace,
+			logger:    log.WithValues("Namespace", namespace),
 		}
+		deactivator.checkUserRecordsForDeactivation()
 	}, period, stopChan)
 }
 
-func checkUserRecordsForDeactivation(namespace string, mgrClient clientlib.Client) error {
-	checkLogger := log.WithValues("Namespace", namespace)
-	checkLogger.Info("listing NSTemplateTier resources")
+type deactivator struct {
+	client    client.Client
+	namespace string
+	logger    logr.Logger
+}
+
+func (d deactivator) checkUserRecordsForDeactivation() {
+
+	d.logger.Info("listing NSTemplateTier resources")
 
 	tiers := toolchainv1alpha1.NSTemplateTierList{}
-	if err := mgrClient.List(context.Background(), &tiers,
-		clientlib.InNamespace(namespace),
-		clientlib.Limit(5),
+	if err := d.client.List(context.Background(), &tiers,
+		clientlib.InNamespace(d.namespace),
 	); err != nil {
-		return errs.Wrap(err, "unable to get MasterUserRecords")
+		d.logger.Error(err, "unable to get MasterUserRecords")
 	}
 
 	log.Info("listed NSTemplateTiers", "count", len(tiers.Items))
 	if len(tiers.Items) == 0 {
 		// no tiers to process
-		return nil
+		return
 	}
 
 	// find MURs that should be deactivated for each tier
 	for _, nsTemplateTier := range tiers.Items {
-		murs := toolchainv1alpha1.MasterUserRecordList{}
 		tier := toolchainv1alpha1.NSTemplateTier{}
-		if err := mgrClient.Get(context.TODO(), types.NamespacedName{
+		if err := d.client.Get(context.TODO(), types.NamespacedName{
 			Namespace: nsTemplateTier.Namespace,
 			Name:      nsTemplateTier.Name,
 		}, &tier); err != nil {
-			return errs.Wrap(err, "unable to get NSTemplateTier")
+			d.logger.Error(err, "unable to get NSTemplateTier")
+			continue
 		}
 
-		// if tier.Spec.DeactivationTimeoutSeconds == nil || len(*tier.Spec.DeactivationTimeoutSeconds) == 0 {
-		// 	continue
-		// }
-
-		// // timeoutDuration, err := time.ParseDuration(*tier.Spec.DeactivationTimeoutSeconds)
-		// timeoutDays, err := strconv.ParseInt(*tier.Spec.DeactivationTimeoutSeconds, 10, 64)
-		// if err != nil {
-		// 	return errs.Wrapf(err, "tier %s deactivation timeout value is invalid")
-		// }
-
-		// timeoutDuration := time.Duration(timeoutDays*24) * time.Hour
-
-		// TODO remove temporary hard-coded timeout duration value
-		timeoutDuration := 20 * time.Second
-		provisionedBefore := fmt.Sprintf("%d", time.Now().Add(-timeoutDuration).Unix())
-
-		log.Info("masteruserrecord label selector", "provisionedBefore", provisionedBefore)
-		matchingLabels, err := murSelector(provisionedBefore)
-		if err != nil {
-			log.Info("error mur selector")
-			return errs.Wrap(err, "unable to get masteruserrecords")
-		}
-		if err = mgrClient.List(context.Background(), &murs,
-			clientlib.InNamespace(namespace),
-			clientlib.Limit(5),
-			matchingLabels,
-		); err != nil {
-			log.Info("error mur list")
-			return errs.Wrap(err, "unable to get masteruserrecords")
-		}
-		log.Info("rajiv: masteruserrecords to deactivate", "count", len(murs.Items))
-
-		for _, mur := range murs.Items {
-			usersignup := &toolchainv1alpha1.UserSignup{}
-			if err := mgrClient.Get(context.TODO(), types.NamespacedName{
-				Namespace: mur.Namespace,
-				Name:      mur.Spec.UserID,
-			}, usersignup); err != nil {
-				log.Error(err, "unable to get usersignup")
-				continue
-			}
-
-			// trigger deactivation on the user signup
-			usersignup.Spec.Deactivated = true
-			mgrClient.Update(context.TODO(), usersignup)
-
-			log.Info("rajiv: found mur", "name", mur.Name, "tier", mur.Spec.UserAccounts[0].Spec.NSTemplateSet.TierName)
-
-		} // end murs loop
-
+		d.deactivateExpiredMursForTier(tier)
 	} // end tiers loop
+}
 
-	return nil
+func (d deactivator) deactivateExpiredMursForTier(tier toolchainv1alpha1.NSTemplateTier) {
+	// if tier.Spec.DeactivationTimeoutSeconds == nil || len(*tier.Spec.DeactivationTimeoutSeconds) == 0 {
+	// 	continue
+	// }
+
+	// // timeoutDuration, err := time.ParseDuration(*tier.Spec.DeactivationTimeoutSeconds)
+	// timeoutDays, err := strconv.ParseInt(*tier.Spec.DeactivationTimeoutSeconds, 10, 64)
+	// if err != nil {
+	// 	return errs.Wrapf(err, "tier %s deactivation timeout value is invalid")
+	// }
+
+	// timeoutDuration := time.Duration(timeoutDays*24) * time.Hour
+
+	// TODO remove temporary hard-coded timeout duration value
+	timeoutDuration := 60 * time.Second
+	expiry := fmt.Sprintf("%d", time.Now().Add(-timeoutDuration).Unix())
+
+	log.Info("masteruserrecord label selector", "expiry", expiry)
+	matchingLabels, err := expiredMurSelector(expiry)
+	if err != nil {
+		d.logger.Error(err, "mur labels invalid")
+	}
+	murs := toolchainv1alpha1.MasterUserRecordList{}
+	if err = d.client.List(context.Background(), &murs,
+		clientlib.InNamespace(d.namespace),
+		clientlib.Limit(5),
+		matchingLabels,
+	); err != nil {
+		d.logger.Error(err, "unable to list masteruserrecords")
+	}
+	log.Info("rajiv: masteruserrecords to deactivate", "count", len(murs.Items))
+
+	for _, mur := range murs.Items {
+		usersignup := &toolchainv1alpha1.UserSignup{}
+		if err := d.client.Get(context.TODO(), types.NamespacedName{
+			Namespace: mur.Namespace,
+			Name:      mur.Spec.UserID,
+		}, usersignup); err != nil {
+			log.Error(err, "unable to get usersignup")
+			continue
+		}
+
+		log.Info("deactivating usersignup for mur", "name", mur.Name, "tier", mur.Spec.UserAccounts[0].Spec.NSTemplateSet.TierName)
+
+		// trigger deactivation on the user signup
+		usersignup.Spec.Deactivated = true
+		d.client.Update(context.TODO(), usersignup)
+
+	} // end murs loop
 }
 
 func getProvisionedTime(mur *toolchainv1alpha1.MasterUserRecord) (int64, error) {
@@ -129,9 +137,10 @@ func getProvisionedTime(mur *toolchainv1alpha1.MasterUserRecord) (int64, error) 
 	return 0, fmt.Errorf("mur does not have %s label", toolchainv1alpha1.MasterUserRecordProvisionedTimeUNIXLabelKey)
 }
 
-func murSelector(provisionedBefore string) (client.MatchingLabelsSelector, error) {
+func expiredMurSelector(expiry string) (client.MatchingLabelsSelector, error) {
+	// retrieve all murs provisioned beyond the expiry period, provisioned timestamps less than the expiry timestamp indicate they are too old
 	selector := labels.NewSelector()
-	provisionedTimeLabel, err := labels.NewRequirement(toolchainv1alpha1.MasterUserRecordProvisionedTimeUNIXLabelKey, selection.LessThan, []string{provisionedBefore})
+	provisionedTimeLabel, err := labels.NewRequirement(toolchainv1alpha1.MasterUserRecordProvisionedTimeUNIXLabelKey, selection.LessThan, []string{expiry})
 	if err != nil {
 		return client.MatchingLabelsSelector{}, err
 	}
